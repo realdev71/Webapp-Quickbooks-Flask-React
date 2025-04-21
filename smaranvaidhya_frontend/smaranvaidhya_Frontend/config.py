86132b254ee0d6a98990cd1ad8f7f6a664de61b6
BoPettry <bobbypettry99@proton.me>
2025-04-21 19:34:13 +0530

    changes

--- smaranvaidhya_frontend/smaranvaidhya_Frontend/config.py
+++ smaranvaidhya_frontend/smaranvaidhya_Frontend/config.py
@@ -1,3 +1,4 @@
 class Config:
     SQLALCHEMY_DATABASE_URI = 'postgresql://healthcare_user:12345678@127.0.0.1:5432/healthcare_db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = True
